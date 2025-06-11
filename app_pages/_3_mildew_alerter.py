c8f547b073d944b5a926713c4a0bbde0d9da59c5
CorDamir <cordamir@gmail.com>
2025-06-11 19:26:54 +0200

    add table report generation and download as csv functionality to ML prediction page

--- app_pages/_3_mildew_alerter.py
+++ app_pages/_3_mildew_alerter.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from src.ML_model_functionality import MildewAlerter, run_analysis
+import pandas as pd
+import io
+from datetime import datetime
 
 
 def mildew_alerter_page():
@@ -28,6 +31,7 @@ def mildew_alerter_page():
 
     st.divider()
     st.subheader("Upload images and check for powdery mildew", divider=True)
+
     uploader_images = st.file_uploader(
         "Upload cherry leaf images here",
         type=["png", "jpg", "jpeg"],
@@ -35,16 +39,48 @@ def mildew_alerter_page():
         label_visibility="collapsed"
     )
 
-    # if any uploaded files
-    if uploader_images:
-        run_model_button = st.button("Run analysis!")
-        # on "run analysis" button press
-        if run_model_button:
-            if model is None:
-                model = MildewAlerter("v1")
-            analysis_results = run_analysis(model, uploader_images)
+    # initial conditions for buttons
+    generate_table_button = False
+    if "analysis_done" not in st.session_state:
+        st.session_state.analysis_done = False
+    if "table_ready" not in st.session_state:
+        st.session_state.table_ready = False
+    if "analysis_results" not in st.session_state:
+        st.session_state.analysis_results = []
+    if "dl_done" not in st.session_state:
+        st.session_state.dl_done = False
 
-            for img, text in analysis_results:
+    # if any files in uploader
+    if uploader_images:
+        col_btn_1, col_btn_2, col_btn_3 = st.columns([1, 1, 1])
+
+        with col_btn_1:
+            # if run analysis button pressed
+            if st.button("Run analysis!"):
+                if model is None:
+                    model = MildewAlerter("v1")
+                st.session_state.analysis_results = run_analysis(
+                                                    model, uploader_images
+                                                    )
+                st.session_state.analysis_done = True
+                st.session_state.table_ready = False
+                st.session_state.dl_done = False
+
+        with col_btn_2:
+            if st.session_state.analysis_done:
+                generate_table_button = st.button("Generate report table")
+
+        # if generate table button pressed
+        if generate_table_button:
+            st.session_state.table_data = generate_table_data(
+                                            st.session_state.analysis_results
+                                            )
+            st.session_state.table_ready = True
+            st.table(st.session_state.table_data)
+
+        # display analysis results
+        elif st.session_state.analysis_done:
+            for img, text in st.session_state.analysis_results:
                 col_img, col_text = st.columns([1, 2])
                 with col_img:
                     st.divider()
@@ -52,3 +88,38 @@ def mildew_alerter_page():
                 with col_text:
                     st.divider()
                     st.write(text)
+
+        with col_btn_3:
+            if st.session_state.analysis_done and st.session_state.table_ready:
+                get_download_button(
+                    st.session_state.table_data,
+                    st.session_state.dl_done
+                    )
+                st.session_state.dl_done = True
+
+
+def generate_table_data(results):
+    table_data = []
+
+    for img, text in results:
+        img_name = img.name
+        pred = "Healthy" if "healthy" in text else "Powdery mildew"
+        table_data.append({"Image name": img_name, "Prediction": pred})
+
+    return pd.DataFrame(table_data)
+
+
+def get_download_button(df, used):
+    temp_ram_storage = io.StringIO()
+    df.to_csv(temp_ram_storage, index=False)
+    csv_data = temp_ram_storage.getvalue().encode("utf-8")
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"mildew_report_{now}.csv"
+
+    return st.download_button(
+        label="Download table data (CSV)",
+        data=csv_data,
+        file_name=file_name,
+        mime="text/csv",
+        disabled=used
+        )
