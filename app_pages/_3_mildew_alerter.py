a5e9a7a367b3b573136bdccc2e17ac0adbf234fd
CorDamir <cordamir@gmail.com>
2025-06-12 05:02:37 +0200

    refactor mildew alerter page code for clarity of st.session use

--- app_pages/_3_mildew_alerter.py
+++ app_pages/_3_mildew_alerter.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit import session_state as sesh
 from src.ML_model_functionality import MildewAlerter, run_analysis
 import pandas as pd
 import io
@@ -6,8 +7,6 @@ from datetime import datetime
 
 
 def mildew_alerter_page():
-    model = None
-
     st.header("Mildew alerter - AI powered classifier")
     st.divider()
 
@@ -18,7 +17,9 @@ def mildew_alerter_page():
         )
     st.divider()
 
-    with st.expander("If needed, sample dataset for testing is available here"):
+    with st.expander(
+        "If needed, sample dataset for testing is available here"
+                    ):
         with open("./outputs/sample_test/sample_cherry_leaves.zip", "rb") as f:
             dl_data = f.read()
 
@@ -32,55 +33,53 @@ def mildew_alerter_page():
     st.divider()
     st.subheader("Upload images and check for powdery mildew", divider=True)
 
-    uploader_images = st.file_uploader(
+    uploaded_imgs = st.file_uploader(
         "Upload cherry leaf images here",
         type=["png", "jpg", "jpeg"],
         accept_multiple_files=True,
         label_visibility="collapsed"
     )
 
-    # initial conditions for buttons
-    generate_table_button = False
-    if "analysis_done" not in st.session_state:
-        st.session_state.analysis_done = False
-    if "table_ready" not in st.session_state:
-        st.session_state.table_ready = False
-    if "analysis_results" not in st.session_state:
-        st.session_state.analysis_results = []
-    if "dl_done" not in st.session_state:
-        st.session_state.dl_done = False
+    # initial session conditions
+    generate_report_button = False
+    if "model" not in sesh:
+        sesh.model = MildewAlerter("v1")
+    if "analysis_done" not in sesh:
+        sesh.analysis_done = False
+    if "table_ready" not in sesh:
+        sesh.table_ready = False
+    if "analysis_results" not in sesh:
+        sesh.analysis_results = []
+    if "dl_button_used" not in sesh:
+        sesh.dl_button_used = False
 
     # if any files in uploader
-    if uploader_images:
+    if uploaded_imgs:
         col_btn_1, col_btn_2, col_btn_3 = st.columns([1, 1, 1])
 
         with col_btn_1:
-            # if run analysis button pressed
+            # if run analysis button pressed enable dl button
+            # and flag analysis done
             if st.button("Run analysis!"):
-                if model is None:
-                    model = MildewAlerter("v1")
-                st.session_state.analysis_results = run_analysis(
-                                                    model, uploader_images
-                                                    )
-                st.session_state.analysis_done = True
-                st.session_state.table_ready = False
-                st.session_state.dl_done = False
+                sesh.analysis_results = run_analysis(sesh.model, uploaded_imgs)
+                sesh.analysis_done = True
+                sesh.table_ready = False
+                sesh.dl_button_used = False
 
         with col_btn_2:
-            if st.session_state.analysis_done:
-                generate_table_button = st.button("Generate report table")
+            # if analysis done display generate report button
+            if sesh.analysis_done:
+                generate_report_button = st.button("Generate report table")
 
-        # if generate table button pressed
-        if generate_table_button:
-            st.session_state.table_data = generate_table_data(
-                                            st.session_state.analysis_results
-                                            )
-            st.session_state.table_ready = True
-            st.table(st.session_state.table_data)
+        # if generate table button pressed flag: table is ready
+        if generate_report_button:
+            sesh.table_data = generate_table_data(sesh.analysis_results)
+            sesh.table_ready = True
+            st.table(sesh.table_data)
 
-        # display analysis results
-        elif st.session_state.analysis_done:
-            for img, text in st.session_state.analysis_results:
+        # analysis done and generate table not pressed
+        elif sesh.analysis_done:
+            for img, text in sesh.analysis_results:
                 col_img, col_text = st.columns([1, 2])
                 with col_img:
                     st.divider()
@@ -90,21 +89,17 @@ def mildew_alerter_page():
                     st.write(text)
 
         with col_btn_3:
-            if st.session_state.analysis_done and st.session_state.table_ready:
-                get_download_button(
-                    st.session_state.table_data,
-                    st.session_state.dl_done
-                    )
-                st.session_state.dl_done = True
+            if sesh.analysis_done and sesh.table_ready:
+                get_download_button(sesh.table_data, sesh.dl_button_used)
+                sesh.dl_button_used = True
 
 
 def generate_table_data(results):
     table_data = []
 
     for img, text in results:
-        img_name = img.name
         pred = "Healthy" if "healthy" in text else "Powdery mildew"
-        table_data.append({"Image name": img_name, "Prediction": pred})
+        table_data.append({"Image name": img.name, "Prediction": pred})
 
     return pd.DataFrame(table_data)
 
